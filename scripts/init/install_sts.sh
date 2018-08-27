cefb7614ff793591d2aaaaf147890e7454ba7931
Andrzej Sydor <andrzej.sydor@gmail.com>
2018-08-27 16:43:35 +0200

    update STS version

--- scripts/init/install_sts.sh
+++ scripts/init/install_sts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-STS=spring-tool-suite-3.9.1.RELEASE-e4.7.1a-linux-gtk-x86_64.tar.gz
+STS=spring-tool-suite-3.9.5.RELEASE-e4.8.0-linux-gtk-x86_64.tar.gz
 mkdir -p ~/dev/app/sts
-wget -N http://download.springsource.com/release/STS/3.9.1.RELEASE/dist/e4.7/$STS -P ~/Downloads
+wget -N http://download.springsource.com/release/STS/3.9.5.RELEASE/dist/e4.8/$STS -P ~/Downloads
 tar -zxvf ~/Downloads/$STS -C ~/dev/app/sts --strip-components=1
-nohup ~/dev/app/sts/sts-3.9.1.RELEASE/STS & > /dev/null 2>&1&
+nohup ~/dev/app/sts/sts-3.9.5.RELEASE/STS & > /dev/null 2>&1&
